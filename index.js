8247e7f9f188a22acdc6669751d19d0198f41da4
Spyrincho <42439682+Spyrincho@users.noreply.github.com>
2019-06-10 15:08:31 +0200

    Added more commands. See code for more

--- index.js
+++ index.js
@@ -7,6 +7,25 @@ client.once('ready', () => {
 })
 
 client.on('message', async message => {
+
+  if (message.content === `${prefix}ping`) {
+    // send back "Pong." to the channel the message was sent in
+    message.channel.send('Pong.');
+  }
+
+  if(message.member.hasPermission(["SEND_MESSAGES"])){
+      
+    if(message.content.startsWith(`${prefix}help`))
+      message.channel.send('This is currently being worked on at this very moment.');
+
+   if(message.content.startsWith(`${prefix}invitebot`))
+      message.channel.send('You can invite me to other servers using this link: https://bit.ly/2I81tBL');
+    
+    if(message.content.startsWith(`${prefix}github`))
+      message.channel.send('This is the github repo: https://github.com/Spyrincho/BeanHammerDiscordBot ');
+    
+  }
+
   if(message.member.hasPermission(['KICK_MEMBERS', 'BAN_MEMBERS'])) {
 
       //console.log(message.content);
@@ -20,14 +39,15 @@ client.on('message', async message => {
     })
   }
 
-  if(message.content.startsWith(`${prefix}ban`)) {
-    //message.channel.send("kick")
-
-    let member = message.mentions.members.first();
-    member.ban().then((member) => {
-      message.channel.send(":wave: " + member.displayName + " has been yeeted out of the fucking universe and will never return!")
-    })
-  }
+    if(message.content.startsWith(`${prefix}ban`)) {
+      //message.channel.send("ban")
+  
+      let member = message.mentions.members.first();
+      member.ban().then((member) => {
+        message.channel.send(":wave: " + member.displayName + " has been yeeted out of the fucking universe and will never return!")
+      })
+    }
+  
 
   }
   
