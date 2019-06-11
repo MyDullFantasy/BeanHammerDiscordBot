5c3277686c9e588f03c533fca6c60b73d4ceda59
Spyrincho <42439682+Spyrincho@users.noreply.github.com>
2019-06-11 17:15:06 +0200

    Added multiple new commands

--- index.js
+++ index.js
@@ -6,28 +6,67 @@ client.once('ready', () => {
   console.log('The bot is ready and fired up for use!!')
 })
 
+client.on('ready', () => {
+  client.user.setStatus('available')
+  client.user.setPresence({
+      game: {
+          name: 'with my code',
+          type: "PLAYING",
+          url: "https://www.twitch.tv/MutedOreo"
+      }
+  });
+});
+
 client.on('message', async message => {
 
-  if (message.content === `${prefix}ping`) {
-    // send back "Pong." to the channel the message was sent in
-    message.channel.send('Pong.');
+  if(message.member.hasPermission(["MANAGE_NICKNAMES"])){
+    if(message.content.startsWith(`${prefix}nickset`)){
+      member.displayName.replace(message.content.replace`${prefix}nickset`, ''.trim());
+    }
   }
 
   if(message.member.hasPermission(["SEND_MESSAGES"])){
-      
-    if(message.content.startsWith(`${prefix}help`))
-      message.channel.send('This is currently being worked on at this very moment.');
-
-    if(message.content.startsWith(`${prefix}invitebot`))
-      message.channel.send('You can invite me to other servers using this link: https://bit.ly/2I81tBL');
-    
-    if(message.content.startsWith(`${prefix}github`))
-      message.channel.send('This is the github repo: https://github.com/Spyrincho/BeanHammerDiscordBot ');
-      
-    if(message.content.startsWith(`${prefix}say`))
-      message.channel.sendMessage(`<@`+message.author.id+`>` + " said: " + '"' + message.content.replace(`${[prefix]}say`,'').trim() + '"');
-    
+    if(message.content.startsWith(`${prefix}sobble`)){
+      message.channel.send('More information about who made the bot is soon to follow.');
+    }
   }
+
+    //Moderation & Fun Commands
+
+  if(message.member.hasPermission(["SEND_MESSAGES"])){
+
+    //if(message.content.startsWith(`${prefix}cat`)){
+      //message.channel.send('https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzEwNC84MTkvb3JpZ2luYWwvY3V0ZS1raXR0ZW4uanBn');
+      //}
+
+    if(message.content.startsWith(`${prefix}help`)){
+      message.channel.send('This is currently being worked on at this very moment.');
+      message.channel.send('But these are the current commands: e!sub [subreddit], e!invite, e!github, e!say [insert text], e!hidesay [insert text], e!kick [@ member], e!ban [@ member] & e!sobble.')
+      }
+
+    if(message.content.startsWith(`${prefix}sub`)){
+      message.channel.send('Have fun on this subreddit!  https://www.reddit.com/r/' + message.content.replace(`${[prefix]}sub`, '').trim());
+      }
+
+    if(message.content.startsWith(`${prefix}invite`)){
+      message.channel.send('You can invite me to other servers using this link: https://bit.ly/2F2R4VP');
+      }
+    
+    if(message.content.startsWith(`${prefix}github`)){
+      message.channel.send('This is the github repo: https://github.com/Spyrincho/BeanHammerDiscordBot ');
+      }
+      
+    if(message.content.startsWith(`${prefix}say`)){
+      message.channel.sendMessage(`<@`+message.author.id+`>` + " said: " + '"' + message.content.replace(`${prefix}say`,'').trim() + '"');
+      }
+
+    if(message.content.startsWith(`${prefix}hidesay`)){
+    message.delete();
+    message.channel.sendMessage(`Anonymous` + " said: " + '"' + message.content.replace(`${prefix}hidesay`,'').trim() + '"');
+      }
+  }
+
+  //Kick & Ban Function 
 
   if(message.member.hasPermission(['KICK_MEMBERS', 'BAN_MEMBERS'])) {
 
